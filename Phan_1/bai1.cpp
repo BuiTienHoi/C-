98bdf05677f0825566253b7e6715a9aa4d126402
BuiTienHoi <cauuthobui17@gmail.com>
2024-11-09 03:38:37 +0700

    change all files

--- Phan_1/bai1.cpp
+++ Phan_1/bai1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     system("chcp 1251>nul");
-    int number;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    system("pause>nul");
     return 0;
 }
